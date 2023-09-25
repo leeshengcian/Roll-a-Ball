0447f71f1ef8c031d1122e7697833ed0604b8d67
leeshengcian <sclee.cs12@nycu.edu.tw>
2023-09-25 12:07:35 +0800

    add UI for score and win
    newly unity use TMPro, rather than UnityEngine.UI

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+//using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    private Rigidbody rb;
+    
     public float speed;
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI winText;
+
+    private Rigidbody rb;
+    private int count;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
     }
 
     private void FixedUpdate()
@@ -27,6 +38,17 @@ public class Player_Controller : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+        }
+    }
+
+    private void setCountText()
+    {
+        countText.text = "Count : " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
